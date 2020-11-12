7e4ab6b842c754c8f1f07931d15bbf2a70a7ba8c
Rachid_Abdellaoui <rachidweb4@gmail.com>
2020-11-12 11:00:32 +0100

    js

---
+++ collection.js
@@ -0,0 +1,73 @@
+const collection = [{
+        title: 'Ligne verte',
+        Réalisateur: 'Frank Darabont',
+        Genre: ['Drame'],
+        link: 'https://www.youtube.com/watch?v=mccs8Ql8m8o',
+        description: "Paul Edgecomb, pensionnaire centenaire d'une maison de retraite, est hanté par ses souvenirs. Gardien-chef du pénitencier de Cold Mountain, en 1935, en Louisiane, il était chargé de veiller au bon déroulement des exécutions capitales au bloc E (la ligne verte) en s'efforçant d'adoucir les derniers moments des condamnés. Parmi eux se trouvait un colosse du nom de John Coffey, accusé du viol et du meurtre de deux fillettes"
+    },
+    {
+        title: 'Casino',
+        Réalisateur: 'Martin Scorsese',
+        Genre: ['Mafia'],
+        link: 'https://www.youtube.com/watch?v=7XpRCdzsW8o&t=12s',
+        description: "Las Vegas, années 70. Ace Rothstein dirige d'une main de fer le Tangiers, hôtel casino parmi les plus prospères de la ville, financé en sous main par le puissant syndicat des camionneurs. Ace est devenu le grand manitou de Las Vegas, secondé par son ami d'enfance, Nicky Santoro. Impitoyable avec les tricheurs, Rothstein se laisse un jour séduire par une virtuose de l'arnaque d'une insolente beauté, Ginger McKenna"
+    },
+    {
+        title: 'Inception',
+        Réalisateur: 'Christopher Nolan',
+        Genre: ['Sience fiction'],
+        link: 'https://www.youtube.com/watch?v=HcoZbHBDHQA&t=2s',
+        description: "Dom Cobb est un voleur expérimenté dans l'art périlleux de `l'extraction' : sa spécialité consiste à s'approprier les secrets les plus précieux d'un individu, enfouis au plus profond de son subconscient, pendant qu'il rêve et que son esprit est particulièrement vulnérable. Très recherché pour ses talents dans l'univers trouble de l'espionnage industriel, Cobb est aussi devenu un fugitif traqué dans le monde entier. Cependant, une ultime mission pourrait lui permettre de retrouver sa vie d'avant."
+    },
+    {
+        title: 'Carbone',
+        Réalisateur: 'Olivier Marchal',
+        Genre: ['Thriller'],
+        link: 'hhttps://www.youtube.com/results?search_query=inception+bande+annonce+francais',
+        description: "Menacé de perdre son entreprise, Antoine Roca, un homme ordinaire, met au point une arnaque qui deviendra le casse du siècle. Rattrapé par le grand banditisme, il lui faudra faire face aux trahisons, meurtres et règlements de compte."
+    },
+    {
+        title: 'Very Bad Trip',
+        Réalisateur: 'Todd Phillips',
+        Genre:['Comédie'],
+        link: 'https://www.youtube.com/watch?v=UBsx-rvu9Uk',
+        description: "Trois jeunes hommes cherchent un ami qui a disparu au cours d'une super fête à Las Vegas. Le trio doit tenter de se souvenir de toutes les mauvaises décisions prises la veille afin de retrouver leur ami et le ramener à temps pour son mariage prévu à Los Angeles."
+    },
+    {
+        title: 'Fight Club',
+        Réalisateur: 'David Fincher',
+        Genre: ['Action'],
+        link: 'https://www.youtube.com/watch?v=7XpRCdzsW8o&t=12s',
+        description: "Jack est un jeune expert en assurance insomniaque, désillusionné par sa vie personnelle et professionnelle. Lorsque son médecin lui conseille de suivre une thérapie afin de relativiser son mal-être, il rencontre dans un groupe d'entraide Marla avec qui il parvient à trouver un équilibre."
+    },
+    {
+        title: 'American History X',
+        Réalisateur: 'Tony Kayer',
+        Genre: ['Action'],
+        link: 'https://www.youtube.com/watch?v=wgRbmDfzBuU&t=11s',
+        description: "Ce film tente d'expliquer l'origine du racisme et de l'extrémisme aux États-Unis. Il raconte l'histoire de Derek qui, voulant venger la mort de son père, abattu par un dealer noir, a épousé les thèses racistes d'un groupuscule de militants d'extrême droite et s'est mis au service de son leader, brutal théoricien prônant la suprématie de la race blanche. Ces théories le mèneront à commettre un double meurtre entraînant son jeune frère, Danny, dans la spirale de la haine."
+    },
+    {
+        title: 'Usual Suspects',
+        Réalisateur: 'Bryan Singer',
+        Genre: ['Thriller policier'],
+        link: 'https://www.youtube.com/watch?v=8LtQOl0RJkI',
+        description: "Interrogé par la police à la suite de l'explosion criminelle d'un cargo, Verbal Kint se met à table : avec quatre autres gangsters, il s'est vu imposer une mission périlleuse par Keyser Söze, un malfrat craint de tous mais que personne ne connaît. Qui est ce mystérieux commanditaire ? Existe-t-il vraiment ? Bryan Singer nous entraîne dans les méandres d'un scénario éblouissant, multipliant les fausses pistes jusqu'au dénouement final."
+    },
+    {
+        title: 'Carlos',
+        Réalisateur: 'Olivier Assayas',
+        Genre: ['Biographical'],
+        link: 'https://www.youtube.com/watch?v=wgRbmDfzBuU&t=11s',
+        description: "Le film est l'histoire d'un révolutionnaire internationaliste, manipulateur et manipulé, porté par les flux de l'histoire de son époque et de ses dérives. Nous le suivrons jusqu'au bout de son chemin, relégué au Soudan où la dictature islamiste, après l'avoir un temps couvert, l'a livré à la police française."
+    },
+    {
+        title: 'Avatar',
+        Réalisateur: 'James Cameronr',
+        Genre: ['Science fiction'],
+        link: 'https://www.youtube.com/watch?v=O1CzgULNRGs',
+        description: "Malgré sa paralysie, Jake Sully, un ancien marine immobilisé dans un fauteuil roulant, est resté un combattant au plus profond de son être. Il est recruté pour se rendre à des années-lumière de la Terre, sur Pandora, où de puissants groupes industriels exploitent un minerai rarissime destiné à résoudre la crise énergétique sur Terre."
+    },
+
+
+]
