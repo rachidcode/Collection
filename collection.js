35c96a6e78ba45a18bf654a1b96cf14c982d175e
Rachid_Abdellaoui <rachidweb4@gmail.com>
2020-11-12 17:10:38 +0100

    commit

--- collection.js
+++ collection.js
@@ -1,76 +1,160 @@
 const collection = [{
         title: 'Ligne verte',
-        Réalisateur: 'Frank Darabont',
+        Réalisateur: 'Réalisateur : Frank Darabont',
+        image: "La_Ligne_verte.jpg",
         Genre: ['Drame'],
         link: 'https://www.youtube.com/watch?v=mccs8Ql8m8o',
+        bande:'Bande Annonce',
         description: "Paul Edgecomb, pensionnaire centenaire d'une maison de retraite, est hanté par ses souvenirs. Gardien-chef du pénitencier de Cold Mountain, en 1935, en Louisiane, il était chargé de veiller au bon déroulement des exécutions capitales au bloc E (la ligne verte) en s'efforçant d'adoucir les derniers moments des condamnés. Parmi eux se trouvait un colosse du nom de John Coffey, accusé du viol et du meurtre de deux fillettes"
     },
     {
         title: 'Casino',
-        Réalisateur: 'Martin Scorsese',
+        Réalisateur: 'Réalisateur : Martin Scorsese',
+        image: "Casino.jpg",
         Genre: ['Mafia'],
         link: 'https://www.youtube.com/watch?v=7XpRCdzsW8o&t=12s',
+        bande:'Bande Annonce',
         description: "Las Vegas, années 70. Ace Rothstein dirige d'une main de fer le Tangiers, hôtel casino parmi les plus prospères de la ville, financé en sous main par le puissant syndicat des camionneurs. Ace est devenu le grand manitou de Las Vegas, secondé par son ami d'enfance, Nicky Santoro. Impitoyable avec les tricheurs, Rothstein se laisse un jour séduire par une virtuose de l'arnaque d'une insolente beauté, Ginger McKenna"
     },
     {
         title: 'Inception',
-        Réalisateur: 'Christopher Nolan',
+        Réalisateur: 'Réalisateur : Christopher Nolan',
+        image: "incep.jpg",
         Genre: ['Sience fiction'],
         link: 'https://www.youtube.com/watch?v=HcoZbHBDHQA&t=2s',
+        bande:'Bande Annonce',
         description: "Dom Cobb est un voleur expérimenté dans l'art périlleux de `l'extraction' : sa spécialité consiste à s'approprier les secrets les plus précieux d'un individu, enfouis au plus profond de son subconscient, pendant qu'il rêve et que son esprit est particulièrement vulnérable. Très recherché pour ses talents dans l'univers trouble de l'espionnage industriel, Cobb est aussi devenu un fugitif traqué dans le monde entier. Cependant, une ultime mission pourrait lui permettre de retrouver sa vie d'avant."
     },
     {
         title: 'Carbone',
-        Réalisateur: 'Olivier Marchal',
+        Réalisateur: 'Réalisateur : Olivier Marchal',
+        image: "Carbone.jpg",
         Genre: ['Thriller'],
         link: 'hhttps://www.youtube.com/results?search_query=inception+bande+annonce+francais',
+        bande:'Bande Annonce',
         description: "Menacé de perdre son entreprise, Antoine Roca, un homme ordinaire, met au point une arnaque qui deviendra le casse du siècle. Rattrapé par le grand banditisme, il lui faudra faire face aux trahisons, meurtres et règlements de compte."
     },
     {
         title: 'Very Bad Trip',
-        Réalisateur: 'Todd Phillips',
-        Genre:['Comédie'],
+        Réalisateur: 'Réalisateur : Todd Phillips',
+        image: "very.jpg",
+        Genre: ['Comédie'],
         link: 'https://www.youtube.com/watch?v=UBsx-rvu9Uk',
+        bande:'Bande Annonce',
         description: "Trois jeunes hommes cherchent un ami qui a disparu au cours d'une super fête à Las Vegas. Le trio doit tenter de se souvenir de toutes les mauvaises décisions prises la veille afin de retrouver leur ami et le ramener à temps pour son mariage prévu à Los Angeles."
     },
     {
         title: 'Fight Club',
-        Réalisateur: 'David Fincher',
+        Réalisateur: 'Réalisateur : David Fincher',
+        image: "fight-club.jpg",
         Genre: ['Action'],
         link: 'https://www.youtube.com/watch?v=7XpRCdzsW8o&t=12s',
+        bande:'Bande Annonce',
         description: "Jack est un jeune expert en assurance insomniaque, désillusionné par sa vie personnelle et professionnelle. Lorsque son médecin lui conseille de suivre une thérapie afin de relativiser son mal-être, il rencontre dans un groupe d'entraide Marla avec qui il parvient à trouver un équilibre."
     },
     {
         title: 'American History X',
-        Réalisateur: 'Tony Kayer',
+        Réalisateur: 'Réalisateur : Tony Kayer',
+        image: "American.jpg",
         Genre: ['Action'],
         link: 'https://www.youtube.com/watch?v=wgRbmDfzBuU&t=11s',
+        bande:'Bande Annonce',
         description: "Ce film tente d'expliquer l'origine du racisme et de l'extrémisme aux États-Unis. Il raconte l'histoire de Derek qui, voulant venger la mort de son père, abattu par un dealer noir, a épousé les thèses racistes d'un groupuscule de militants d'extrême droite et s'est mis au service de son leader, brutal théoricien prônant la suprématie de la race blanche. Ces théories le mèneront à commettre un double meurtre entraînant son jeune frère, Danny, dans la spirale de la haine."
     },
     {
         title: 'Usual Suspects',
-        Réalisateur: 'Bryan Singer',
+        Réalisateur: 'Réalisateur : Bryan Singer',
+        image: "usual.jpeg",
         Genre: ['Thriller policier'],
         link: 'https://www.youtube.com/watch?v=8LtQOl0RJkI',
+        bande:'Bande Annonce',
         description: "Interrogé par la police à la suite de l'explosion criminelle d'un cargo, Verbal Kint se met à table : avec quatre autres gangsters, il s'est vu imposer une mission périlleuse par Keyser Söze, un malfrat craint de tous mais que personne ne connaît. Qui est ce mystérieux commanditaire ? Existe-t-il vraiment ? Bryan Singer nous entraîne dans les méandres d'un scénario éblouissant, multipliant les fausses pistes jusqu'au dénouement final."
     },
     {
         title: 'Carlos',
-        Réalisateur: 'Olivier Assayas',
+        Réalisateur: 'Réalisateur : Olivier Assayas',
+        image: "Carlos.jpg",
         Genre: ['Biographical'],
         link: 'https://www.youtube.com/watch?v=wgRbmDfzBuU&t=11s',
+        bande:'Bande Annonce',
         description: "Le film est l'histoire d'un révolutionnaire internationaliste, manipulateur et manipulé, porté par les flux de l'histoire de son époque et de ses dérives. Nous le suivrons jusqu'au bout de son chemin, relégué au Soudan où la dictature islamiste, après l'avoir un temps couvert, l'a livré à la police française."
     },
     {
         title: 'Avatar',
-        Réalisateur: 'James Cameronr',
+        Réalisateur: 'Réalisateur :James Cameronr',
+        image: "Avatar.jpg",
         Genre: ['Science fiction'],
         link: 'https://www.youtube.com/watch?v=O1CzgULNRGs',
+        bande:'Bande Annonce',
         description: "Malgré sa paralysie, Jake Sully, un ancien marine immobilisé dans un fauteuil roulant, est resté un combattant au plus profond de son être. Il est recruté pour se rendre à des années-lumière de la Terre, sur Pandora, où de puissants groupes industriels exploitent un minerai rarissime destiné à résoudre la crise énergétique sur Terre."
     },
 
 
 ]
 // console.log(collection)
-document.querySelector("section").innerHTML = ''
+document.querySelector(".container-fluid").innerHTML = ''
 
+const monContainer = document.querySelector(".container-fluid");
+
+const row = document.createElement('div');
+row.className = "row row-col-1 row-col-md-4 mx-auto";
+
+monContainer.appendChild(row);
+
+for (let element of collection) {
+
+
+    const col = document.createElement('div');
+    col.className = "col-12 col-sm-6 col-md-4 col-xl-3 mb-4";
+
+    const p3 = document.createElement('div');
+    p3.className = "p-3 mb-2 bg-white text-dark";
+
+    const images = document.createElement('img');
+    images.className = "card-img-top";
+    images.src = element.image;
+
+    const carte = document.createElement('div');
+    carte.className = "card-body";
+
+    const h5 = document.createElement('h5');
+    h5.className = "card-title";
+    h5.textContent = element.title;
+
+    const badge1 = document.createElement('span');
+    badge1.className = "badge badge-pill badge-primary";
+    badge1.textContent = element.Genre;
+
+    const badge2 = document.createElement('span');
+    badge2.className = "badge badge-success";
+    badge2.textContent = element.Réalisateur;
+
+    const parag = document.createElement('p');
+    parag.className = "overflow-auto";
+    parag.style = "height: 10rem;";
+    parag.textContent = element.description;
+
+    const link = document.createElement('div');
+    link.className = "lien";
+
+    const ba = document.createElement('a');
+    ba.className = "btn btn-primary";
+    ba.href = element.link;
+    ba.textContent = element.bande;
+    
+   
+
+
+
+    row.appendChild(col);
+    col.appendChild(p3);
+    p3.appendChild(images);
+    p3.appendChild(carte)
+    carte.appendChild(h5);
+    carte.appendChild(badge1);
+    carte.appendChild(badge2);
+    carte.appendChild(parag);
+    carte.appendChild(link);
+    link.appendChild(ba);
+
+}
